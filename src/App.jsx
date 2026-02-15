0f67058b938bfe7dea9bd7763943d317f0eedc32
anand-exe7 <anandsiva0317@gmail.com>
2026-02-16 01:03:12 +0530

    akshay dashboard merge (app.jsx , main.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
-import Landing from "./pages/Landing";
-import "./index.css";
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import DashboardPage from "./pages/dashboard/page"
+import Landing from "./pages/Landing"
 
-export default function App() {
-  return <Landing />;
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#F8FAF5] selection:bg-[#4F772D]/30">
+      
+      <Navbar />
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </main>
+
+      
+    </div>
+  )
 }
+
+export default App
