1f84a6a255b91c183f5d107e85b7fbdf56c5b1d9
anand-exe7 <anandsiva0317@gmail.com>
2026-02-16 18:19:09 +0530

    /tharun module 4 part completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        background: '#E8EEF2',
-        card: '#C7D3DD',
-        primaryAccent: '#77B6EA',
-        secondaryAccent: '#D6C9C9',
-      },
-      boxShadow: {
-        glow: '0 0 10px #77B6EA',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
